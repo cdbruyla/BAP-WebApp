7b684687c40bc8cf5413329ec04ece7c6c7e71e2
cdbruyla <Cedric.Bruylandt@UGent.be>
2022-04-16 20:17:36 +0200

    HomepageEdit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+import { BlobServiceClient } from "@azure/storage-blob";
+
+const connStr = "<connection string>";
+const blobServiceClient = BlobServiceClient.fromConnectionString(connStr);
+
+
 if (environment.production) {
   enableProdMode();
 }
